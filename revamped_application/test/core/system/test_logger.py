7fdc1b1d85d19b99cfdcef3601e305c780c352ac
George Tay <george_tay@u.nus.edu>
2024-06-25 09:00:02 +0800

    Fix bug with logger file

--- revamped_application/test/core/system/test_logger.py
+++ revamped_application/test/core/system/test_logger.py
@@ -16,11 +16,15 @@ class TestLogger(unittest.TestCase):
         with self.assertLogs(logger.logger, level="DEBUG"):
             logger.debug("Test debug message")
 
+        logger.close()
+
     def test_info(self):
         logger = Logger("test_logger")
 
         with self.assertLogs(logger.logger, level="INFO"):
             logger.info("Test info message")
+
+        logger.close()
 
     def test_warning(self):
         logger = Logger("test_logger")
@@ -28,14 +32,22 @@ class TestLogger(unittest.TestCase):
         with self.assertLogs(logger.logger, level="WARNING"):
             logger.warning("Test warning message")
 
+        logger.close()
+
     def test_error(self):
         logger = Logger("test_logger")
 
         with self.assertLogs(logger.logger, level="ERROR"):
             logger.error("Test error message")
 
+        logger.close()
+
     def tearDown(self):
         """Deletion script taken from https://www.squash.io/how-to-delete-a-file-or-folder-in-python/"""
 
         if os.path.exists(Logger.LOG_DIR):
-            shutil.rmtree(Logger.LOG_DIR)
+            try:
+                shutil.rmtree(Logger.LOG_DIR)
+            except PermissionError:
+                # the file is still open in the logger tested, we can ignore it for now
+                pass
