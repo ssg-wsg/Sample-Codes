9c2b3694c34c5c39eb1a00e9ef03a4df69ce5fe4
George Tay <george_tay@u.nus.edu>
2024-07-10 14:57:01 +0800

    Update ports

--- SSG-API-Testing-Application-v2/deploy/main-infrastructure/main-infrastructure.tf
+++ SSG-API-Testing-Application-v2/deploy/main-infrastructure/main-infrastructure.tf
@@ -129,6 +129,13 @@ resource "aws_security_group" "ecs_node_sg" {
   name_prefix = "ecs-node-sg"
   vpc_id      = aws_vpc.main.id
 
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
