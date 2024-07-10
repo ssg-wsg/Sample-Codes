99b1c50cc44e27ac071744ee32ae29c4c61996d2
George Tay <george_tay@u.nus.edu>
2024-07-10 10:55:04 +0800

    Update Capacity Name

--- SSG-API-Testing-Application-v2/deploy/main-infrastructure/main-infrastructure.tf
+++ SSG-API-Testing-Application-v2/deploy/main-infrastructure/main-infrastructure.tf
@@ -193,7 +193,7 @@ resource "aws_autoscaling_group" "ecs" {
 
 # Create Capacity Provider
 resource "aws_ecs_capacity_provider" "main" {
-  name = "ecs-ec2"
+  name = "ssg-ecs-ec2"
 
   auto_scaling_group_provider {
     auto_scaling_group_arn         = aws_autoscaling_group.ecs.arn
