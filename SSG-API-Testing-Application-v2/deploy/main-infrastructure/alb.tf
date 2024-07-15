747873abb59fb027b1b81a8196feea163b91a0ad
George Tay <george_tay@u.nus.edu>
2024-07-15 13:17:36 +0800

    Remove TLS/SSL Support

--- SSG-API-Testing-Application-v2/deploy/main-infrastructure/alb.tf
+++ SSG-API-Testing-Application-v2/deploy/main-infrastructure/alb.tf
@@ -17,20 +17,6 @@ resource "aws_alb_listener" "alb_default_listener_http" {
   }
 }
 
-# Create HTTPS listener
-resource "aws_alb_listener" "alb_default_listener_https" {
-  load_balancer_arn = aws_alb.alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  certificate_arn   = aws_acm_certificate.cert.arn
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_alb_target_group.service_target_group.arn
-  }
-}
-
 # Create TG
 resource "aws_alb_target_group" "service_target_group" {
   name                 = "${module.constants.namespace}-targetGroup"
