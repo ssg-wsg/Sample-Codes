2cccd482e93e24eadcac294305ed31b5b26fc192
George Tay <george_tay@u.nus.edu>
2024-07-17 20:28:02 +0800

    Revert changes made to ALB Security Group

--- SSG-API-Testing-Application-v2/deploy/main-infrastructure/security-groups.tf
+++ SSG-API-Testing-Application-v2/deploy/main-infrastructure/security-groups.tf
@@ -4,19 +4,11 @@ resource "aws_security_group" "ec2" {
   description = "Security group for EC2 instances"
   vpc_id      = aws_vpc.default.id
 
-  # permit only HTTP traffic from ALB
   ingress {
-    from_port       = 80
-    to_port         = 80
+    from_port       = 1024
+    to_port         = 65535
     protocol        = "tcp"
     security_groups = [aws_security_group.alb.id]
-  }
-
-  ingress {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_host.id]
   }
 
   ingress {
