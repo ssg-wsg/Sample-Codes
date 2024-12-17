de6f126600767335566fec360d9685564859669f
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2024-12-17 16:04:54 +0800

    add newline after argument

--- SSG-API-Testing-Application-v2/lambda/deploy/secretsmanager.tf
+++ SSG-API-Testing-Application-v2/lambda/deploy/secretsmanager.tf
@@ -15,7 +15,7 @@ resource "aws_secretsmanager_secret" "example_secret" {
 
 resource "aws_secretsmanager_secret_version" "example_secret_version" {
   secret_id     = aws_secretsmanager_secret.example_secret.id
-  secret_string = json.encode({
+  secret_string = json.encode ({
     test_secret = var.secret_value
   })
 }
