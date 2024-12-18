f7356659fc2f3b37d18b3c26b0e8a4a76f7124b4
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2024-12-18 16:15:13 +0800

    add terraform code and workflow file

--- SSG-API-Testing-Application-v2/lambda/deploy/ssm-parameter-store.tf
+++ SSG-API-Testing-Application-v2/lambda/deploy/ssm-parameter-store.tf
@@ -1,17 +1,35 @@
-# Define input variable for the secret value
-variable "secret_value" {
-  description = "The value of the secret to be stored"
-  type        = string
-  sensitive   = true
+# Define input variables for the secret value, 
+# env variable should have key value pairs like below
+# {
+#   "db_password": {
+#     "value": "${{ secrets.DB_PASSWORD }}",
+#     "description": "The database password for the application"
+#   },
+#   "api_key": {
+#     "value": "${{ secrets.API_KEY }}",
+#     "description": "The API key for accessing external services"
+#   }
+# }
+
+variable "secrets" {
+  description = "A map of secrets with their values and descriptions"
+  type = map(object({
+    value       = string
+    description = string
+  }))
+  sensitive = true
+}
+
+resource "aws_ssm_parameter" "secrets" {
+  for_each = var.secrets
+
+  name        = each.key
+  value       = each.value.value
+  description = each.value.description
+
+  type = "SecureString"
 }
 
 provider "aws" {
   region = "ap-southeast-1"
-}
-
-resource "aws_ssm_parameter" "example_secret" {
-  name        = "/SampleApp/example_secrets/example" # Name of the secret
-  description = "The parameter description"
-  type        = "SecureString"
-  value       = var.secret_value
 }
